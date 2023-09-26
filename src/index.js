24505bd810f6fb3a030907a25667e5ea0593a841
Namiko01 <vitaliknam01@gmail.com>
2023-09-27 01:40:59 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined){
+    return [];
+  }else{
+    let arr = [];
+    let result = [];
+    // развернули нечетные массивы внутри
+    matrix.forEach((elem, index) => {
+        if(index % 2 != 0){
+            arr.push(elem.reverse());
+        }
+        else{
+            arr.push(elem);
+        }
+    });
+    // переобразовываем в один
+    arr.map(elem => {
+        for(let i = 0; i < elem.length; i++){
+            result.push(elem[i])
+        }
+    })
+    return result;
+  }
 }
